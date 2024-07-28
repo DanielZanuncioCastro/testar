9b7c9568517bc3019c88084c6870f7aacca8b5e2
DanielZanuncioCastro <danielzanunciodiascastro@hotmail.com>
2024-07-28 21:52:27 +0000

    soma

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  int soma = 0;
+  for (int i = 1; i <= 10; i++)
+    {
+      soma += i;
+    }
+  std::cout << "Hello World!\n" << endl;
+  std::cout << soma;
+  
 }
