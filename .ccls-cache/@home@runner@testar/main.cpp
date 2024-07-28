4cd9f58f18a81afa416124771c406e4f18a171c3
DanielZanuncioCastro <danielzanunciodiascastro@hotmail.com>
2024-07-28 21:56:54 +0000

    rodar

---
+++ .ccls-cache/@home@runner@testar/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int soma = 0;
+  for (int i = 1; i <= 10; i++)
+    {
+      soma += i;
+    }
+  std::cout << "Hello World!\n" << endl;
+  std::cout << soma;
+  
+}
